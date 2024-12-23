df8c1ccc52c0e4e63ba02810fd77b0d5f372d24f
Ananya Prabhu <ananya.prabhu@edu.turkuamk.fi>
2024-12-23 20:10:20 +0200

    Added git-test-project and changed code in git_first.py

--- git_first.py
+++ git_first.py
@@ -1,11 +1,5 @@
-# Author : Ananya Prabhu
-# File : git_first.py
+#Author : Ananya Prabhu
+#File : git_first.py
 
-# original code
-# print("I am a beginner with Git")
-
-# modified code
-# print("I am a bit familiar with Git")
-
-# 2nd Modified code
-print("I am doing stashing and Undoing the stashing part")
+#modified code
+print("I am a beginner with Git")
